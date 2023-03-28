815e9eb60be885887dc5813ac41cb975db0d6d3f
CircuitSacul <circuitsacul@icloud.com>
2023-03-28 02:00:02 -0400

    formatting

--- wires/utils.py
+++ wires/utils.py
@@ -2,12 +2,14 @@ import typing as t
 
 T = t.TypeVar("T")
 
+
 def unwrap(value: T | None, msg: str = "expected value, got None") -> T:
     assert value is not None, msg
     return value
 
+
 def clip(value: str, size: int) -> str:
     assert size > 3, "size must be at least 3"
     if len(value) > size:
-        return value[0:size-3] + "..."
+        return value[0 : size - 3] + "..."
     return value
