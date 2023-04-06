20e5a6a4eb3cc9bb769940f7cdec1e18d0607c75
CircuitSacul <circuitsacul@icloud.com>
2023-04-06 11:33:57 -0400

    add cooldowns

--- wires/plugins/highlights/plugin.py
+++ wires/plugins/highlights/plugin.py
@@ -1,6 +1,9 @@
+from datetime import timedelta
+
 import crescent
 import hikari
 import regex_rs
+from floodgate import FixedMapping
 
 from wires.database.models import Highlight
 from wires.utils import clip, unwrap
@@ -9,16 +12,22 @@ from .. import Plugin
 
 plugin = Plugin()
 
+TRIGGER_COOLDOWN: "FixedMapping[int]" = FixedMapping(3, timedelta(minutes=10))
+ACTIVE_COOLDOWN: "FixedMapping[int]" = FixedMapping(1, timedelta(minutes=5))
+
 
 @plugin.include
 @crescent.event
 async def on_message(event: hikari.GuildMessageCreateEvent) -> None:
+    ACTIVE_COOLDOWN.reset(event.author_id)
+    ret = ACTIVE_COOLDOWN.trigger(event.author_id)
+    assert ret is None, "reset failed"
+
     if not event.content:
         return
 
     highlights = (
-        await Highlight.fetch_query()
-        .where(guild_id=event.guild_id)
+        await Highlight.fetch_query().where(guild_id=event.guild_id)
         .where(Highlight.user_id.neq(event.author_id))
         .fetchmany()
     )
@@ -53,6 +62,10 @@ async def on_message(event: hikari.GuildMessageCreateEvent) -> None:
             if hl.content not in event.content:
                 continue
 
+        if not ACTIVE_COOLDOWN.can_trigger(hl.user_id):
+            continue
+        if TRIGGER_COOLDOWN.trigger(hl.id):
+            continue
         notifications.setdefault(hl.user_id, []).append(clip(hl.content, 12))
 
     guild = unwrap(event.get_guild())
