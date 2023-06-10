a343cd13007b46517f0cc906f66f473413fd3acf
CircuitSacul <circuitsacul@icloud.com>
2023-06-10 02:09:18 -0700

    check permissions before sending highlights

--- wires/plugins/highlights/plugin.py
+++ wires/plugins/highlights/plugin.py
@@ -1,8 +1,10 @@
+import logging
 from datetime import timedelta
 
 import crescent
 import hikari
 import regex_rs
+import toolbox
 from floodgate import FixedMapping
 
 from wires.database.models import Highlight
@@ -12,8 +14,41 @@ from .. import Plugin
 
 plugin = Plugin()
 
+LOG = logging.getLogger(__file__)
+
 TRIGGER_COOLDOWN: "FixedMapping[int]" = FixedMapping(3, timedelta(minutes=10))
 ACTIVE_COOLDOWN: "FixedMapping[tuple[int, int]]" = FixedMapping(1, timedelta(minutes=5))
+
+
+async def has_permission(guild_id: int, user_id: int, channel_id: int) -> bool:
+    member = plugin.app.cache.get_member(guild_id, user_id)
+    if not member:
+        try:
+            member = await plugin.app.rest.fetch_member(guild_id, user_id)
+        except hikari.NotFoundError:
+            return False
+
+    channel = plugin.app.cache.get_guild_channel(channel_id)
+    if not channel:
+        # must be a thread, because hikari caches all other channel types
+        # entirely.
+        thread = plugin.app.rest.fetch_channel(channel_id)
+
+        if isinstance(thread, hikari.GuildThreadChannel):
+            return await has_permission(guild_id, user_id, thread.parent_id)
+
+        elif isinstance(thread, hikari.PermissibleGuildChannel):
+            LOG.error("Non-thread channel was not cached. {}", thread)
+            # we can still use it though
+            channel = thread
+
+        else:
+            LOG.error("Non-thread channel was non-permissible.", thread)
+            # nothing we can do at this point
+            return False
+
+    permissions = toolbox.calculate_permissions(member, channel)
+    return hikari.Permissions.VIEW_CHANNEL in permissions
 
 
 @plugin.include
@@ -68,6 +103,10 @@ async def on_message(event: hikari.GuildMessageCreateEvent) -> None:
             continue
         if TRIGGER_COOLDOWN.trigger(hl.id):
             continue
+
+        if not await has_permission(event.guild_id, hl.user_id, event.channel_id):
+            continue
+
         notifications.setdefault(hl.user_id, []).append(clip(hl.content, 12))
 
     guild = unwrap(event.get_guild())
