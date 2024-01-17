cbd1c9e54c64cb256f624aacb1f7d3e7b576f331
MithicSpirit <rpc01234@gmail.com>
2024-01-17 14:10:53 -0500

    feat(highlights): show source channel name

--- wires/plugins/highlights/plugin.py
+++ wires/plugins/highlights/plugin.py
@@ -113,13 +113,17 @@ async def on_message(event: hikari.GuildMessageCreateEvent) -> None:
     guild = unwrap(event.get_guild())
     for user, triggers in notifications.items():
         channel = await plugin.app.rest.create_dm_channel(user)
+        footer = guild.name
+        if (source_channel := event.get_channel()):
+            if (source_channel_name := source_channel.name):
+                footer += f" | {source_channel_name}"
         embed = (
             hikari.Embed(
                 title="Jump",
                 description=event.content,
                 url=event.message.make_link(event.guild_id),
             )
-            .set_footer(guild.name, icon=guild.icon_url)
+            .set_footer(footer, icon=guild.icon_url)
             .set_author(name=event.author.username, icon=event.author.avatar_url)
         )
         await channel.send(f"Highlights triggered: {', '.join(triggers)}", embed=embed)
